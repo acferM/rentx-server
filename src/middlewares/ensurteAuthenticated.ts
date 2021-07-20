35695be44861c162d44a469fe0a6cc8802f06e51
acferM <matheus.acfer@nave.org.br>
2021-07-20 02:32:10 -0300

    Authentication ensurance middleware created

---
+++ src/middlewares/ensurteAuthenticated.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepositoryu';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('Token missing');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      '7752cd8a007bc0221e654b13bfeb814d',
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error('User does not exists');
+    }
+
+    next();
+  } catch {
+    throw new Error('Invalid token');
+  }
+}
